d132a7dea203c4497ee84e0bb18434fd57669b18
Keerthi <Keerthi_V03@infosys.com>
2020-06-23 09:15:25 -0700

    ee

--- src/main/java/com/example/Ratingdemo/RatingdemoApplication.java
+++ src/main/java/com/example/Ratingdemo/RatingdemoApplication.java
@@ -2,27 +2,11 @@ package com.example.Ratingdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import io.jaegertracing.Configuration;
-import io.jaegertracing.internal.samplers.ProbabilisticSampler;
-import io.opentracing.Tracer;
 
 @SpringBootApplication
 public class RatingdemoApplication {
-
-	@Bean
-	public io.opentracing.Tracer jagerTracer(){
-		
-		//return (Tracer) new Configuration("spring-boot").getTracer();
-		
-		 return (Tracer) new Configuration("spring-boot")
-			        .getTracer();
-		
-		/*return new Configuration("spring-boot", 
-				new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE,1]), new Configuration.ReporterConfiguration()).getTracer();*/
-		
-	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RatingdemoApplication.class, args);
 	}
